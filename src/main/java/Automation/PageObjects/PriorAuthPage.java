f8573b811a05a45a60ebeb37685cf29d8ae03c8b
MIRRA\tsaimounika <tsaimounika@mirrahealthcare.com>
2022-08-03 10:07:39 +0530

    done changes

--- src/main/java/Automation/PageObjects/PriorAuthPage.java
+++ src/main/java/Automation/PageObjects/PriorAuthPage.java
@@ -14,28 +14,43 @@ public class PriorAuthPage extends SeleniumUtils {
     String tabPriorAuth = "//*[@id = 'nav-prior-authorization-tab']";
     String lstPriorAuth = "//app-prior-authorization//span[@ref = 'eText']";
     String txtPriorAuthSearchFields = "//app-prior-authorization//*[@class = 'ag-floating-filter-input']";
-    String btnFooterFields = "//*[@class='button-padding-left footer']/button";
+    String btnFooterFields = "//*[@class='footer footer-flex']//button";
 
 
+    //  Scenario: Verify user navigate to Prior Authorization tab from Global Search
     public void userClicksPriorAuthorizationTab() throws InterruptedException {
         clickElement(tabPriorAuth);
         threadSleep(1000);
     }
 
-    public void verifyUserNavigatesToPriorAuthTab(String expPriorAuthTab){
+    public void verifyUserNavigatesToPriorAuthTab(String expPriorAuthTab) {
         Assert.assertEquals(expPriorAuthTab, findElementByXpath(tabPriorAuth).getText());
     }
 
-    public void verifyUserViewsColumnFieldsUnderPriorAuth(DataTable columnFields){
+    //  Scenario: Verify Prior Authorization column fields
+    public void verifyUserViewsColumnFieldsUnderPriorAuth(DataTable columnFields) {
         List<String> fieldsExp = columnFields.asList();
-        List<String> ActFields = findElementsByXpath(lstPriorAuth)
-                .stream().map((e) -> e.getText().trim()).collect(Collectors.toList());
-        System.out.println("Fields in Prior Authorization tab :" + ActFields);
-        System.out.println("Expected fields are: " + fieldsExp);
-        Assert.assertEquals(ActFields, fieldsExp);
+        List<WebElement> ActColumnFields = findElementsByXpath(lstPriorAuth);
+        List<String> columnFieldsForCompare = new ArrayList<>();
+        System.out.println("Size " + ActColumnFields.size());
+        for (WebElement column : ActColumnFields) {
+            scrollIntoView(column, driver);
+            String text = column.getText();
+            columnFieldsForCompare.add(text);
+        }
+        System.out.println("Fields in Prior Authorization tab :" + columnFieldsForCompare);
+        System.out.println("Expected fields are : " + fieldsExp);
+        for (String exp : fieldsExp) {
+            if (columnFieldsForCompare.contains(exp)) {
+                Assert.assertTrue(true);
+            } else {
+                Assert.fail(exp + " is not listed in actual list");
+            }
+        }
     }
 
-    public void verifySearchFieldsUnderEachColumn(){
+    //  Scenario: Verify Search box under each column and able to enter search criteria
+    public void verifySearchFieldsUnderEachColumn() {
         List<WebElement> ActSearchFields = findElementsByXpath(txtPriorAuthSearchFields);
         for (WebElement column : ActSearchFields) {
             scrollIntoView(column, driver);
@@ -44,6 +59,7 @@ public class PriorAuthPage extends SeleniumUtils {
         }
     }
 
+    //  Scenario: Validate footer section in Prior Authorization tab
     public void userViewsFooterSectionInPriorAuth(DataTable footerFields) {
         List<String> fieldsExp = footerFields.asList();
         List<String> ActFields = findElementsByXpath(btnFooterFields)
--- src/test/java/Automation/StepDefinitions/PriorAuthStepDef.java
+++ src/test/java/Automation/StepDefinitions/PriorAuthStepDef.java
@@ -7,6 +7,8 @@ import io.cucumber.java.en.When;
 
 public class PriorAuthStepDef extends PriorAuthPage {
 
+
+    //  Scenario: Verify user navigate to Prior Authorization tab from Global Search
     @When("user clicks on the Prior Authorization tab")
     public void user_clicks_on_the_prior_authorization_tab() throws InterruptedException {
         userClicksPriorAuthorizationTab();
@@ -17,16 +19,19 @@ public class PriorAuthStepDef extends PriorAuthPage {
         verifyUserNavigatesToPriorAuthTab(expPriorAuthTab);
     }
 
+    //  Scenario: Verify Prior Authorization column fields
     @Then("user should be able to view the below column fields under Prior Authorization tab")
     public void user_should_be_able_to_view_the_below_column_fields_under_prior_authorization_tab(DataTable columnFields) {
         verifyUserViewsColumnFieldsUnderPriorAuth(columnFields);
     }
 
+    //  Scenario: Verify Search box under each column and able to enter search criteria
     @Then("user should be able to view the Search fields under each column")
     public void user_should_be_able_to_view_the_search_fields_under_each_column() {
         verifySearchFieldsUnderEachColumn();
     }
 
+    //  Scenario: Validate footer section in Prior Authorization tab
     @Then("user views the footer section in Prior Authorization page")
     public void user_views_the_footer_section_in_prior_authorization_page(DataTable footerFields) {
         userViewsFooterSectionInPriorAuth(footerFields);
