e6cbd6049b873a3f3e70ec619a6d340c4194dfaf
Sshada <sshada@mirrahealthcare.com>
2022-08-05 10:21:57 +0530

    scenarios added in FFS

--- src/main/java/Automation/PageObjects/FFSProfessionalPage.java
+++ src/main/java/Automation/PageObjects/FFSProfessionalPage.java
@@ -1,9 +1,13 @@
 package Automation.PageObjects;
 
 import Automation.Utilities.SeleniumUtils;
+import io.cucumber.datatable.DataTable;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.Color;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class FFSProfessionalPage extends SeleniumUtils {
@@ -13,10 +17,38 @@ public class FFSProfessionalPage extends SeleniumUtils {
     String eleClaimNumber = "(//div[@class='ag-pinned-left-cols-container']//a)[1]";
     String eleVerticalColorForCorrected = "(//div[@col-id='isCorrected']//span)[6]";
     String eleCorrectedInfoTopGrid = "//div//span[@class='ColorBall']//following::span[contains(text(),'Corrected')]";
-    //String elePatientNameColorCode = "((//div[contains(@class,'ag-cell-focus') and @col-id='memberFullName'])[1]//span)[3]";
     String elePatientNameColorCode = "((//div[contains(@class,'ag-cell ag-cell-not-inline-editing ag-cell-normal-height') and @col-id='memberFullName'])[1]//span)[3]";
     String eleCOBInMemberHouseInfoTopGrid = "//span[@class='ColorBall_COBInMemberHouse']//following::span[contains(text(),'COB (In Member House)')]";
     String eleCOBNotInMemberHouseInfoTopGrid = "//span[@class='ColorBall_COB_NoninMemberHouse']//following::span[contains(text(),'COB (Not in Member House)')]";
+    String lstColumnFields = "//ag-grid-angular[@id='pendGrid']//div[@class='ag-header-cell-label']//span[text()]";
+    String lstColumnFieldsExtra = "//ag-grid-angular[@id='pendGrid']//div[@class='ag-header-cell-label']//span[text()]";
+    String tabPendState = "(//button[@class='nav-link active'])[1]";
+    String lstQueues = "//div[@id='nav-tab']//button";
+    String elePendQueuePage = "(//span[@class='ag-cell-value']//app-view-claim-render)[1]";
+    String eleVerticalColorForUncleanPendClaim = "(//div[@col-id='IsClean']//span)[6]";
+    String eleUncleanInfoTopGrid = "//div//span[@class='ColorBall_UnClean']//following::span[contains(text(),'Un-clean')]";
+    String tabPayerReview = "//button[@id='nav-payerreview-details-tab']";
+    String txtPayerReviewClaimNumber = "(//input[@aria-label='Claim Number Filter Input'])[3]";
+    String elePayerReviewClaimNumber="//*[@id='payerReviewGrid']//div[contains(@class,'ag-cell-last-left-pinned')]//a";
+    String eleVerticalColorForUncleanPayerReviewClaim = "(//*[@id='payerReviewGrid']//div[contains(@class,'ag-cell-normal-height')]//span)[4]";
+    String tabManagementReview = "//button[@id='nav-mangementreview-details-tab']";
+    String txtManagementReviewClaimNumber = "(//input[@aria-label='Claim Number Filter Input'])[4]";
+    String eleManagementReviewClaimNumber="//*[@id='mangementReviewGrid']//div[contains(@class,'ag-cell-last-left-pinned')]//a";
+    String eleVerticalColorForUncleanManagementReviewClaim = "(//*[@id='mangementReviewGrid']//div[contains(@class,'ag-cell-normal-height')]//span)[4]";
+    String tabApproved = "//button[@id='nav-approved-details-tab']";
+    String txtApprovedClaimNumber = "(//input[@aria-label='Claim Number Filter Input'])[5]";
+    String eleApprovedClaimNumber="//*[@id='approvedGrid']//div[contains(@class,'ag-cell-last-left-pinned')]//a";
+    String eleVerticalColorForUncleanApprovedClaim = "(//*[@id='approvedGrid']//div[contains(@class,'ag-cell-normal-height')]//span)[4]";
+    String tabDenied = "//button[@id='nav-denied-details-tab']";
+    String txtDeniedClaimNumber = "(//input[@aria-label='Claim Number Filter Input'])[6]";
+    String eleDeniedClaimNumber="//*[@id='deniedGrid']//div[contains(@class,'ag-cell-last-left-pinned')]//a";
+    String eleVerticalColorForUncleanDeniedClaim = "(//*[@id='deniedGrid']//div[contains(@class,'ag-cell-normal-height')]//span)[4]";
+    String tabPreBatch = "//button[@id='nav-perbatch-details-tab']";
+    String txtPreBatchClaimNumber = "(//input[@aria-label='Claim Number Filter Input'])[7]";
+    String elePreBatchClaimNumber="//*[@id='prebatchdGrid']//div[contains(@class,'ag-cell-last-left-pinned')]//a";
+    String eleVerticalColorForUncleanPreBatchClaim = "(//*[@id='prebatchdGrid']//div[contains(@class,'ag-cell-normal-height')]//span)[4]";
+
+
 
     private static String expClaimNumber = "";
 
@@ -75,7 +107,7 @@ public class FFSProfessionalPage extends SeleniumUtils {
         Assert.assertTrue(value);
     }
 
-    //Scenario: Verify color code for COB(Not In Member House) in FFS Professional page
+    //Scenario: Verify color code for COB Not In Member House in FFS Professional page
     public void enterCOBNotInMemberHouseClaimNumber() throws InterruptedException {
         threadSleep(30000);
         expClaimNumber = prop.getProperty("cobNotInMemberHouse");
@@ -85,6 +117,216 @@ public class FFSProfessionalPage extends SeleniumUtils {
     }
     public void verifyCOBNotInMemberHouseInformationOnTopOfGrid() {
         boolean value = isDisplayed(eleCOBNotInMemberHouseInfoTopGrid);
+        Assert.assertTrue(value);
+    }
+
+    //Scenario:Verify colour coding for Unclean status claims under Pend bucket in FFS professional page
+    public void enterUnCleanStatusPendClaimNumber() throws InterruptedException {
+        threadSleep(30000);
+        expClaimNumber = prop.getProperty("unCleanStatusPendClaimNumber");
+        findElementAndSendKeys(findElementByXpath(txtClaimNumber), expClaimNumber);
+        threadSleep(1000);
+        sendKeysUsingKeyboardInput(txtClaimNumber);
+    }
+
+    public void verifyVerticalColorIndicationForUncleanPendClaim(String expColorIndication){
+        String actColorCode = getColorCodeForBackground(eleVerticalColorForUncleanPendClaim);
+        System.out.println("actual color code :" + actColorCode);
+        Assert.assertEquals(expColorIndication, actColorCode);
+    }
+    public void verifyUncleanInformationOnTopOfGrid() {
+        boolean value = isDisplayed(eleUncleanInfoTopGrid);
+        Assert.assertTrue(value);
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Payer Review bucket in FFS professional page
+    public void clickOnPayerReviewBucket(){
+        clickElement(tabPayerReview);
+    }
+
+    public void enterUnCleanStatusPayerReviewClaimNumber() throws InterruptedException {
+        threadSleep(30000);
+        expClaimNumber = prop.getProperty("unCleanStatusPayerReviewClaimNumber");
+        findElementAndSendKeys(findElementByXpath(txtPayerReviewClaimNumber), expClaimNumber);
+        threadSleep(1000);
+        sendKeysUsingKeyboardInput(txtPayerReviewClaimNumber);
+    }
+    public void validatePayerReviewClaimNumberResult() {
+        explicitVisibilityOfWait(findElementByXpath(elePayerReviewClaimNumber), 5);
+        String actClaimNumber = getText(elePayerReviewClaimNumber);
+        System.out.println("actual claimNumber :" + actClaimNumber);
+        Assert.assertEquals(expClaimNumber, actClaimNumber);
+    }
+    public void verifyVerticalColorIndicationForUncleanPayerReviewClaim(String expColorIndication){
+        String actColorCode = getColorCodeForBackground(eleVerticalColorForUncleanPayerReviewClaim);
+        System.out.println("actual color code :" + actColorCode);
+        Assert.assertEquals(expColorIndication, actColorCode);
+    }
+    //Scenario: Verify colour coding for Unclean status claims under Management Review bucket in FFS professional page
+    public void clickOnManagementReviewBucket(){
+        clickElement(tabManagementReview);
+    }
+
+    public void enterUnCleanStatusManagementReviewClaimNumber() throws InterruptedException {
+        threadSleep(30000);
+        expClaimNumber = prop.getProperty("unCleanStatusManagementReviewClaimNumber");
+        findElementAndSendKeys(findElementByXpath(txtManagementReviewClaimNumber), expClaimNumber);
+        threadSleep(1000);
+        sendKeysUsingKeyboardInput(txtManagementReviewClaimNumber);
+    }
+    public void validateManagementReviewClaimNumberResult() {
+        explicitVisibilityOfWait(findElementByXpath(eleManagementReviewClaimNumber), 5);
+        String actClaimNumber = getText(eleManagementReviewClaimNumber);
+        System.out.println("actual claimNumber :" + actClaimNumber);
+        Assert.assertEquals(expClaimNumber, actClaimNumber);
+    }
+    public void verifyVerticalColorIndicationForUncleanManagementReviewClaim(String expColorIndication){
+        String actColorCode = getColorCodeForBackground(eleVerticalColorForUncleanManagementReviewClaim);
+        System.out.println("actual color code :" + actColorCode);
+        Assert.assertEquals(expColorIndication, actColorCode);
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Approved bucket in FFS professional page
+    public void clickOnApprovedBucket(){
+        clickElement(tabApproved);
+    }
+
+    public void enterUnCleanStatusApprovedClaimNumber() throws InterruptedException {
+        threadSleep(30000);
+        expClaimNumber = prop.getProperty("unCleanStatusApprovedClaimNumber");
+        findElementAndSendKeys(findElementByXpath(txtApprovedClaimNumber), expClaimNumber);
+        threadSleep(1000);
+        sendKeysUsingKeyboardInput(txtApprovedClaimNumber);
+    }
+    public void validateApprovedClaimNumberResult() {
+        explicitVisibilityOfWait(findElementByXpath(eleApprovedClaimNumber), 5);
+        String actClaimNumber = getText(eleApprovedClaimNumber);
+        System.out.println("actual claimNumber :" + actClaimNumber);
+        Assert.assertEquals(expClaimNumber, actClaimNumber);
+    }
+    public void verifyVerticalColorIndicationForUncleanApprovedClaim(String expColorIndication){
+        String actColorCode = getColorCodeForBackground(eleVerticalColorForUncleanApprovedClaim);
+        System.out.println("actual color code :" + actColorCode);
+        Assert.assertEquals(expColorIndication, actColorCode);
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Denied bucket in FFS professional page
+    public void clickOnDeniedBucket(){
+        clickElement(tabDenied);
+    }
+
+    public void enterUnCleanStatusDeniedClaimNumber() throws InterruptedException {
+        threadSleep(30000);
+        expClaimNumber = prop.getProperty("unCleanStatusDeniedClaimNumber");
+        findElementAndSendKeys(findElementByXpath(txtDeniedClaimNumber), expClaimNumber);
+        threadSleep(1000);
+        sendKeysUsingKeyboardInput(txtDeniedClaimNumber);
+    }
+    public void validateDeniedClaimNumberResult() {
+        explicitVisibilityOfWait(findElementByXpath(eleDeniedClaimNumber), 5);
+        String actClaimNumber = getText(eleDeniedClaimNumber);
+        System.out.println("actual claimNumber :" + actClaimNumber);
+        Assert.assertEquals(expClaimNumber, actClaimNumber);
+    }
+    public void verifyVerticalColorIndicationForUncleanDeniedClaim(String expColorIndication){
+        String actColorCode = getColorCodeForBackground(eleVerticalColorForUncleanDeniedClaim);
+        System.out.println("actual color code :" + actColorCode);
+        Assert.assertEquals(expColorIndication, actColorCode);
+    }
+    //Scenario: Verify colour coding for Unclean status claims under Pre Batch bucket in FFS professional page
+    public void clickOnPreBatchBucket(){
+        clickElement(tabPreBatch);
+    }
+
+    public void enterUnCleanStatusPreBatchClaimNumber() throws InterruptedException {
+        threadSleep(30000);
+        expClaimNumber = prop.getProperty("unCleanStatusPreBatchClaimNumber");
+        findElementAndSendKeys(findElementByXpath(txtPreBatchClaimNumber), expClaimNumber);
+        threadSleep(1000);
+        sendKeysUsingKeyboardInput(txtPreBatchClaimNumber);
+    }
+    public void validatePreBatchClaimNumberResult() {
+        explicitVisibilityOfWait(findElementByXpath(elePreBatchClaimNumber), 5);
+        String actClaimNumber = getText(elePreBatchClaimNumber);
+        System.out.println("actual claimNumber :" + actClaimNumber);
+        Assert.assertEquals(expClaimNumber, actClaimNumber);
+    }
+    public void verifyVerticalColorIndicationForUncleanPreBatchClaim(String expColorIndication){
+        String actColorCode = getColorCodeForBackground(eleVerticalColorForUncleanPreBatchClaim);
+        System.out.println("actual color code :" + actColorCode);
+        Assert.assertEquals(expColorIndication, actColorCode);
+    }
+
+
+
+    //Scenario: Validate user able to view all the column fields for Pend bucket in FFS Professional screen
+    public void verifyPendStateByDefault(String expState) throws InterruptedException{
+        threadSleep(20000);
+        String actState=findElementByXpath(tabPendState).getAttribute("class");
+        if(actState.contains(expState)){
+            Assert.assertTrue(true);
+        }else{
+            Assert.assertTrue(false);
+        }
+
+
+    }
+
+    public void verifyFFSProfessionalPendColumnFields(DataTable columnList) throws InterruptedException {
+        List<String> expColumnList = columnList.asList();
+        List<WebElement> actColumnFields = findElementsByXpath(lstColumnFields);
+        List<String> actualColumnFieldsForCompare = new ArrayList<>();
+        for (WebElement column : actColumnFields) {
+            threadSleep(1000);
+            scrollIntoView(column, driver);
+            String text = column.getText();
+            actualColumnFieldsForCompare.add(text);
+        }
+        System.out.println("actual column fields " + actualColumnFieldsForCompare);
+        System.out.println("expected column fields " + expColumnList);
+        for (String expColumn : expColumnList) {
+            if (actualColumnFieldsForCompare.contains(expColumn)) {
+                Assert.assertTrue(true);
+            } else {
+                Assert.fail(expColumn + " column is not as expected");
+            }
+        }
+    }
+
+    public void verifyQueuesInFFSProfessional(DataTable queueList) throws InterruptedException{
+        List<String> expQueueList = queueList.asList();
+        List<WebElement> actQueueFields = findElementsByXpath(lstQueues);
+        List<String> actualQueueFieldsForCompare = new ArrayList<>();
+        for (WebElement column : actQueueFields) {
+            threadSleep(1000);
+            scrollIntoView(column, driver);
+            String text = column.getText();
+            String[] queueData=text.split(" ");
+            if(queueData.length==2) {
+                actualQueueFieldsForCompare.add(queueData[0]);
+            }else if(queueData.length==3){
+                actualQueueFieldsForCompare.add(queueData[0]+" "+queueData[1]);
+            }else if(queueData.length==4){
+                actualQueueFieldsForCompare.add(queueData[0]+" "+queueData[1]+" "+queueData[2]);
+            }else{
+                actualQueueFieldsForCompare.add(queueData[0].substring(0,8));
+            }
+        }
+        System.out.println("actual queue fields " + actualQueueFieldsForCompare);
+        System.out.println("expected queue fields " + expQueueList);
+        for (String expQueue : expQueueList) {
+            if (actualQueueFieldsForCompare.contains(expQueue)) {
+                Assert.assertTrue(true);
+            } else {
+                Assert.fail(expQueue + " queue is not as expected");
+            }
+        }
+
+    }
+
+    public void verifyUserByDefaultInPendStateResultsPage() throws InterruptedException {
+        threadSleep(20000);
+        boolean value=isDisplayed(elePendQueuePage);
         Assert.assertTrue(value);
     }
 
--- src/test/java/Automation/StepDefinitions/FFSProfessionalStepDef.java
+++ src/test/java/Automation/StepDefinitions/FFSProfessionalStepDef.java
@@ -1,6 +1,7 @@
 package Automation.StepDefinitions;
 
 import Automation.PageObjects.FFSProfessionalPage;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -62,5 +63,137 @@ public class FFSProfessionalStepDef extends FFSProfessionalPage {
     @Then("color indication information for COB Not In Member House should be available on top of grid")
     public void color_indication_information_for_cob_not_in_member_house_should_be_available_on_top_of_grid() {
         verifyCOBNotInMemberHouseInformationOnTopOfGrid();
+    }
+
+    //Scenario:Verify colour coding for Unclean status claims under Pend bucket in FFS professional page
+    @When("user enters Unclean status claim in search criteria in Pend bucket")
+    public void user_enters_unclean_status_claim_in_search_criteria_in_pend_bucket() throws InterruptedException {
+        enterUnCleanStatusPendClaimNumber();
+    }
+    @Then("user should able to view Unclean status claim in FFS Professional Grid")
+    public void user_should_able_to_view_unclean_status_claim_in_ffs_professional_grid() {
+        validateClaimNumberResult();
+    }
+    @Then("user should able to see vertical line beside unclean status Pend claim number with color code {string} in FFS Professional")
+    public void user_should_able_to_see_vertical_line_beside_unclean_status_Pend_claim_number_with_color_code_in_ffs_professional(String colorCode) {
+        verifyVerticalColorIndicationForUncleanPendClaim(colorCode);
+    }
+    @Then("color indication information for Unclean should be available on top of grid in FFS Professional")
+    public void color_indication_information_for_unclean_should_be_available_on_top_of_grid_in_ffs_professional() {
+        verifyUncleanInformationOnTopOfGrid();
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Payer Review bucket in FFS professional page
+    @When("user clicks on Payer Review bucket")
+    public void user_clicks_on_payer_review_bucket() {
+        clickOnPayerReviewBucket();
+
+    }
+    @When("user enters Unclean status claim in search criteria in Payer Review bucket")
+    public void user_enters_unclean_status_claim_in_search_criteria_in_payer_review_bucket() throws InterruptedException {
+        enterUnCleanStatusPayerReviewClaimNumber();
+    }
+    @Then("user should able to view Unclean status Payer Review claim in FFS Professional Grid")
+    public void user_should_able_to_view_unclean_status_payer_review_claim_in_ffs_professional_grid() {
+        validatePayerReviewClaimNumberResult();
+    }
+    @Then("user should able to see vertical line beside unclean status Payer Review claim number with color code {string} in FFS Professional")
+    public void user_should_able_to_see_vertical_line_beside_unclean_status_payer_review_claim_number_with_color_code_in_ffs_professional(String colorCode) {
+        verifyVerticalColorIndicationForUncleanPayerReviewClaim(colorCode);
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Management Review bucket in FFS professional page
+    @When("user clicks on Management Review bucket")
+    public void user_clicks_on_management_review_bucket() {
+        clickOnManagementReviewBucket();
+    }
+    @When("user enters Unclean status claim in search criteria in Management Review bucket")
+    public void user_enters_unclean_status_claim_in_search_criteria_in_management_review_bucket() throws InterruptedException {
+        enterUnCleanStatusManagementReviewClaimNumber();
+    }
+    @Then("user should able to view Unclean status Management Review claim in FFS Professional Grid")
+    public void user_should_able_to_view_unclean_status_management_review_claim_in_ffs_professional_grid() {
+        validateManagementReviewClaimNumberResult();
+    }
+    @Then("user should able to see vertical line beside unclean status Management Review claim number with color code {string} in FFS Professional")
+    public void user_should_able_to_see_vertical_line_beside_unclean_status_management_review_claim_number_with_color_code_in_ffs_professional(String colorCode) {
+        verifyVerticalColorIndicationForUncleanManagementReviewClaim(colorCode);
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Approved bucket in FFS professional page
+    @When("user clicks on Approved bucket")
+    public void user_clicks_on_approved_bucket() {
+        clickOnApprovedBucket();
+    }
+    @When("user enters Unclean status claim in search criteria in Approved bucket")
+    public void user_enters_unclean_status_claim_in_search_criteria_in_approved_bucket() throws InterruptedException {
+        enterUnCleanStatusApprovedClaimNumber();
+    }
+    @Then("user should able to view Unclean status Approved claim in FFS Professional Grid")
+    public void user_should_able_to_view_unclean_status_approved_claim_in_ffs_professional_grid() {
+        validateApprovedClaimNumberResult();
+    }
+    @Then("user should able to see vertical line beside unclean status Approved claim number with color code {string} in FFS Professional")
+    public void user_should_able_to_see_vertical_line_beside_unclean_status_approved_claim_number_with_color_code_in_ffs_professional(String colorCode) {
+        verifyVerticalColorIndicationForUncleanApprovedClaim(colorCode);
+    }
+    //Scenario: Verify colour coding for Unclean status claims under Denied bucket in FFS professional page
+
+    @When("user clicks on Denied bucket")
+    public void user_clicks_on_denied_bucket() {
+        clickOnDeniedBucket();
+    }
+    @When("user enters Unclean status claim in search criteria in Denied bucket")
+    public void user_enters_unclean_status_claim_in_search_criteria_in_denied_bucket() throws InterruptedException {
+        enterUnCleanStatusDeniedClaimNumber();
+    }
+    @Then("user should able to view Unclean status Denied claim in FFS Professional Grid")
+    public void user_should_able_to_view_unclean_status_denied_claim_in_ffs_professional_grid() {
+        validateDeniedClaimNumberResult();
+    }
+    @Then("user should able to see vertical line beside unclean status Denied claim number with color code {string} in FFS Professional")
+    public void user_should_able_to_see_vertical_line_beside_unclean_status_denied_claim_number_with_color_code_in_ffs_professional(String colorCode) {
+        verifyVerticalColorIndicationForUncleanDeniedClaim(colorCode);
+    }
+
+    //Scenario: Verify colour coding for Unclean status claims under Pre Batch bucket in FFS professional page
+    @When("user clicks on Pre Batch bucket")
+    public void user_clicks_on_pre_batch_bucket() {
+        clickOnPreBatchBucket();
+    }
+    @When("user enters Unclean status claim in search criteria in Pre Batch bucket")
+    public void user_enters_unclean_status_claim_in_search_criteria_in_pre_batch_bucket() throws InterruptedException {
+        enterUnCleanStatusPreBatchClaimNumber();
+    }
+    @Then("user should able to view Unclean status Pre Batch claim in FFS Professional Grid")
+    public void user_should_able_to_view_unclean_status_pre_batch_claim_in_ffs_professional_grid() {
+        validatePreBatchClaimNumberResult();
+    }
+    @Then("user should able to see vertical line beside unclean status Pre Batch claim number with color code {string} in FFS Professional")
+    public void user_should_able_to_see_vertical_line_beside_unclean_status_pre_batch_claim_number_with_color_code_in_ffs_professional(String colorCode) {
+        verifyVerticalColorIndicationForUncleanPreBatchClaim(colorCode);
+    }
+
+
+
+    //Scenario: Validate user able to view all the column fields for Pend bucket in FFS Professional screen
+    @When("user views Pend bucket by default should be {string}")
+    public void user_views_pend_bucket_by_default_should_be(String state) throws InterruptedException{
+        verifyPendStateByDefault(state);
+    }
+    @Then("user able to view following column fields")
+    public void user_able_to_view_following_column_fields(DataTable columnList) throws InterruptedException {
+        verifyFFSProfessionalPendColumnFields(columnList);
+    }
+
+    @Then("user should be able to view following queue detail fields")
+    public void user_should_be_able_to_view_following_queue_detail_fields(DataTable queueList) throws InterruptedException{
+        verifyQueuesInFFSProfessional(queueList);
+    }
+
+    //Scenario: Verify by default user should be in the Pend state in FFS Professional screen
+    @Then("by default user should be in Pend state results page")
+    public void by_default_user_should_be_in_pend_state_results_page() throws InterruptedException {
+        verifyUserByDefaultInPendStateResultsPage();
     }
 }
