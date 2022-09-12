81de50e32a270553787eb63f0833565aa4555c5b
MIRRA\tsaimounika <tsaimounika@mirrahealthcare.com>
2022-09-12 13:47:02 +0530

    done

--- src/test/java/Automation/StepDefinitions/ARLedgerStepDef.java
+++ src/test/java/Automation/StepDefinitions/ARLedgerStepDef.java
@@ -2,7 +2,6 @@ package Automation.StepDefinitions;
 
 import Automation.PageObjects.ARLedgerPage;
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
