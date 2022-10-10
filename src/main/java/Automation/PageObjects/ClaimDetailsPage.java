e301a87a7059df3f5138926ddfdc89c463e64aef
MIRRA\tsaimounika <tsaimounika@mirrahealthcare.com>
2022-10-10 17:31:35 +0530

    done modifications

--- src/main/java/Automation/PageObjects/ClaimDetailsPage.java
+++ src/main/java/Automation/PageObjects/ClaimDetailsPage.java
@@ -69,8 +69,7 @@ public class ClaimDetailsPage extends SeleniumUtils {
     }
 
     public void userClicksOnClaimNumber() {
-        explicitInvisibilityOfElementWithTextWait(By.xpath(tabPend), 60, "Pend ()");
-        explicitElementClickableWaitByXpath(lnkClaimNumber, 50);
+        explicitElementClickableWaitByXpath(lnkClaimNumber, 60);
         clickElement(lnkClaimNumber);
     }
 
@@ -218,6 +217,7 @@ public class ClaimDetailsPage extends SeleniumUtils {
     //  Scenario: Verify footer section in Claim Summary details page
     public void userViewsFooterSectionInClaimSummaryDetails(DataTable footerFields) {
         List<String> footerFieldsExp = footerFields.asList();
+        explicitElementClickableWaitByXpath(btnFooterFields, 40);
         List<WebElement> ActFooterFields = findElementsByXpath(btnFooterFields);
         List<String> fieldsForCompare = new ArrayList<>();
         System.out.println("Size" + ActFooterFields.size());
@@ -883,6 +883,7 @@ public class ClaimDetailsPage extends SeleniumUtils {
     //  Scenario: Verify user able to view 7-Corrected claim submission type in Claim Details page
     public void verifyClaimSubmissionTypeAsCorrected(){
         expClaimSubmission = prop.getProperty("claimSubmissionCorrected");
+        explicitElementClickableWaitByXpath(lblClaimSubmissionCorrected, 50);
         Assert.assertEquals(expClaimSubmission, findElementByXpath(lblClaimSubmissionCorrected).getText());
     }
 
@@ -893,6 +894,7 @@ public class ClaimDetailsPage extends SeleniumUtils {
     }
 
     public void verifyClaimSubmissionType(String type){
+        explicitElementClickableWaitByXpath(eleClaimSubmission, 20);
         Assert.assertEquals(type, findElementByXpath(eleClaimSubmission).getText());
     }
 }
