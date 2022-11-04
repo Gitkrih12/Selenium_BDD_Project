f823a3be765494e07eb8e030ffa88607d29d58f1
MIRRA\tsaimounika <tsaimounika@mirrahealthcare.com>
2022-11-04 12:36:24 +0530

    done

--- src/test/java/Automation/TestRunner/JUnitTestRunnerTest.java
+++ src/test/java/Automation/TestRunner/JUnitTestRunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 glue = "Automation/StepDefinitions",
                 dryRun = false,
                 monochrome = true,
-                tags = "@52277",
+                tags = "@52275",
                 plugin = {"pretty", "html:TestReports/TestResults.html",
                           "json:TestReports/TestResults.json",
                           "junit:TestReports/TestResults.xml",
